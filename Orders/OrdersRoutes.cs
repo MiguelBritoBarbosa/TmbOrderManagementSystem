96dd86ac8dc86ce3f0984faa9f4ab9e897e04397
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2025-02-08 14:36:43 -0300

    adding orders routes

---
+++ Orders/OrdersRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace TmbOrderManagementSystem.Api.Orders
+{
+    public static class OrdersRoutes
+    {
+        public static void AddOrdersRoutes(this WebApplication app)
+        {
+            app.MapPost("orders", () => "orders");
+            app.MapGet("orders", () => "orders");
+            app.MapGet("orders/id", () => "orders");
+        }
+    }
+}
