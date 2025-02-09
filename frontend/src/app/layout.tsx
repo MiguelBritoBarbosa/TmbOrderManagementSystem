be51801a2f7d29fe190d0fb56be445af02bf4e32
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2025-02-09 10:42:28 -0300

    Creating frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "@/styles/globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['500', '600', '700', '800', '900'],
+    variable: '--font-montserrat',
+});
+
+export const metadata: Metadata = {
+    title: "TMB Order Management System",
+    description: "A Concept Test for full stack developer vacancy",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="pt-br">
+            <body
+                className={`${montserrat.className}`}
+            >
+                <SidebarProvider defaultOpen={true}>
+                    <AppSidebar />
+                    <main className="w-full">
+                        <SidebarTrigger />
+                        {children}
+                    </main>
+                </SidebarProvider>
+            </body>
+        </html>
+    );
+}
