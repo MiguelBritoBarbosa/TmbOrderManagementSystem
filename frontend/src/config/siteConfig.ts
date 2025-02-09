3678e4a062bb40556eaee403e33499d0f736e10d
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2025-02-09 16:32:46 -0300

    Creating site config

---
+++ frontend/src/config/siteConfig.ts
@@ -0,0 +1,3 @@
+export const SITE_NAME: string = process.env.NEXT_PUBLIC_SITE_NAME ?? '';
+export const SITE_URL: string = process.env.NEXT_PUBLIC_SITE_URL ?? '';
+export const API_URL: string = process.env.NEXT_PUBLIC_API_URL ?? '';
