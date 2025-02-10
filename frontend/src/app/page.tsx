e89265a6eab40491b437d13fcb3b9e57662331c2
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2025-02-09 23:15:59 -0300

    adding logo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 export default function Home() {
     return (
         <section className="justify-self-center self-center grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
-            <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <div className="flex flex-col gap-8 row-start-2 items-center">
                 <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
+                    className=" dark:invert"
+                    src="/tmb-logo.png"
+                    alt="TMB logo"
+                    width={160}
+                    height={75}
                     priority
                 />
                 <ol className="list-inside list-decimal text-sm text-center sm:text-left">
                     <li className="mb-2">
-                        Create a new order here: <Link href="/orders/create">Create order</Link>
+                        Create a new order here: <Link className="text-blue-500 hover:text-blue-700 transition-colors font-semibold" href="/orders/create">Create order</Link>
                     </li>
-                    <li>See all orders created here: <Link href="/orders">Orders</Link></li>
+                    <li>See all orders created here: <Link className="text-blue-500 hover:text-blue-700 transition-colors font-semibold" href="/orders">Orders</Link></li>
                 </ol>
             </div>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
