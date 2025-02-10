15e0bbb7a95d3dc2cd5f3fc35569aa41335f8161
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2025-02-10 02:52:51 -0300

    fixing post requisition

--- frontend/src/config/data/orders/postOrder.ts
+++ frontend/src/config/data/orders/postOrder.ts
@@ -3,7 +3,7 @@ import { API_URL } from '@/config/siteConfig';
 import { fetchJson } from '@/utils/featchJson';
 
 export async function postOrder(payload: {}): Promise<Order> {
-    const url = `${API_URL}/orders`;
+    const url = `${API_ROOT_URL}/orders`;
     const order = await fetchJson<Order>(url, 'POST', payload);
     return order ?? null;
 }
--- frontend/src/config/siteConfig.ts
+++ frontend/src/config/siteConfig.ts
@@ -1,3 +1,4 @@
 export const SITE_NAME: string = process.env.NEXT_PUBLIC_SITE_NAME ?? '';
 export const SITE_URL: string = process.env.NEXT_PUBLIC_SITE_URL ?? '';
 export const API_URL: string = process.env.NEXT_PUBLIC_API_URL ?? '';
+export const API_ROOT_URL: string = process.env.NEXT_PUBLIC_API_ROOT_URL ?? '';
