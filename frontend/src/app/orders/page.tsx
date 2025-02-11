9f168afdeefb7288f517ebcfd57bfc5ba30b9439
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2025-02-10 23:06:19 -0300

    adding login in front to listening the webSocket

--- frontend/src/app/orders/page.tsx
+++ frontend/src/app/orders/page.tsx
@@ -1,20 +1,61 @@
+"use client";
 import { columns } from "@/components/dataTable/columns";
 import { DataTable } from "@/components/dataTable/data-table";
-import { getAllOrders } from "@/config/data/orders/getAllOrders"
+import { getAllOrders } from "@/config/data/orders/getAllOrders";
 import { formatOrders } from "@/config/domain/orders/formatOrders";
+import { Order } from "@/config/domain/orders/orders";
+import { API_ROOT_URL } from "@/config/siteConfig";
+import { useEffect, useState } from "react";
+import * as signalR from "@microsoft/signalr";
+import { FakeTable } from "@/components/dataTable/fake-table";
 
-export const dynamic = 'force-dynamic'
+export default function Orders() {
+    const [formattedOrders, setFormattedOrders] = useState<Order[]>([]);
+    const [loading, setLoading] = useState(true);
 
-export default async function Orders() {
-    const orders = await getAllOrders();
-    const formattedOrders = formatOrders(orders);
+    useEffect(() => {
+        getAllOrders()
+            .then(data => {
+                setFormattedOrders(formatOrders(data));
+                setLoading(false);
+            });
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(`${API_ROOT_URL}/orderHub`, {
+                withCredentials: true
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        connection.start().catch((err) => console.error(err));
+
+        connection.on("ReceiveOrderUpdate", (updatedOrder: Order) => {
+            setLoading(true);
+            setFormattedOrders((prevOrders) =>
+                prevOrders.map((order) =>
+                    order.id === updatedOrder.id ? formatOrders([updatedOrder])[0] : order
+                )
+            );
+            setTimeout(() => {
+                setLoading(false);
+            }, 500);
+        });
+
+        return () => {
+            connection.stop();
+        };
+    }, []);
 
     return (
         <section className="w-full justify-self-center self-center grid items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
             <div className="w-full flex flex-col gap-8 justify-center items-center sm:items-start">
                 <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Listing all Orders</h1>
-                <DataTable columns={columns} data={formattedOrders} />
+                {loading ? (
+                    <FakeTable columns={columns} data={Array.from({ length: 10 })} />
+                ) : (
+                    <DataTable columns={columns} data={formattedOrders} />
+                )}
             </div>
         </section>
-    )
+    );
 }
---
+++ frontend/src/components/dataTable/fake-table.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { Skeleton } from "../ui/skeleton"
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+}
+
+export function FakeTable<TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+        <>
+            <div className="w-full rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row: any) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell: any) => (
+                                        <TableCell key={cell.id}>
+                                            <Skeleton className="h-4 w-full" />
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+        </>
+    )
+}
